891356ae5b00cd53a86816e05dd49d002e89ce50
jpssantiago <jps_santiago@outlook.com>
2022-06-13 13:17:50 -0300

    feat: versao apresentada na aula 13/06/2022

--- executavel.bat
+++ executavel.bat
@@ -1,5 +1,5 @@
 cls
-SET CLASSPATH=.;C:\Users\jps_s\dev\anhembi\compiladores\n2-compiladores\antlr-4.9.3-complete.jar;%CLASSPATH%
+SET CLASSPATH=.;antlr-4.9.3-complete.jar;%CLASSPATH%
 java org.antlr.v4.Tool Gramatica.g4
 javac *.java
 java Main.java < input.txt > outputs/Code.java
